e571fac86ae0c2f81da5a3146466cd6149208392
Rustam Ibragimov <exploit2002@gmail.com>
2015-09-08 19:20:59 +1100

    Come to the chapter 8.4.6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Login user and redirect to the user's show page
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -15,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
